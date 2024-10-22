2f3f9ec0b95a118ef2c96fb1f06089b9c5978ee7
jagathda <mail.jagath@gmail.com>
2024-10-22 10:26:36 +0300

    Added Security groups

--- main.tf
+++ main.tf
@@ -10,5 +10,31 @@ resource "aws_vpc" "my_vpc" {
 # Subnets for the VPC
 resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0."
+  cidr_block = "10.0.1.0"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0"
+}
+
+# Security groups
+resource "aws_security_group" "fargate_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = [ aws_security_group.fargate_sg.id ] # Allow traffic from ECS tasks
+  }
 }
