5c70bc17cf8c9fb681981d2bfc9aa362f83f0f0f
jagathda <mail.jagath@gmail.com>
2024-10-22 10:17:08 +0300

    Added Subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Subnets for the VPC
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0."
+}
