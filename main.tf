8550e539b2373b79f56a9bca7a1f61b89b951f91
jagathda <mail.jagath@gmail.com>
2024-10-25 10:43:43 +0300

    Added IAM role for Lambda exec role

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "aws_lambda_function" "ecs_invoker_lambda" {
   function_name = "ecs_invoker_lambda"
   handler = "index.handler"
   runtime = "nodejs18.x"
-  role = 
+  role = aws_iam_role.lambda_exec_role.arn
 
   environment {
     variables = {
@@ -169,4 +169,23 @@ resource "aws_lambda_function" "ecs_invoker_lambda" {
   # S3 bucket and key for Lambda function code
     s3_bucket = "lambda-code-bucket"
     s3_key = "lambda-function.zip"
+}
+
+# IAM role for Lambda execution role
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "lambda-execution-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
